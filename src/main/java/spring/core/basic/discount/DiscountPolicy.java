e140d207de42e86a35d0b0bc2cfd9447b8e5676c
Sudongk <aa88sd@naver.com>
2023-07-11 13:41:08 +0900

    feat: 할인 정책 인터페이스와 고정 할인 정책 구현체 생성

---
+++ src/main/java/spring/core/basic/discount/DiscountPolicy.java
@@ -0,0 +1,14 @@
+package spring.core.basic.discount;
+
+import spring.core.basic.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @param member
+     * @param price
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
---
+++ src/main/java/spring/core/basic/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package spring.core.basic.discount;
+
+import spring.core.basic.member.Grade;
+import spring.core.basic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private final int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        }
+
+        return 0;
+    }
+}
